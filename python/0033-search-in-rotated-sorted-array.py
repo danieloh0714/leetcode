b11e8fbe526686d7a62a4a3a122d99d9a6056019
Danny Oh <dannyoh0316@gmail.com>
2021-12-15 13:37:54 -0600

    python 0033 search in rotated array

---
+++ python/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,20 @@
+# Time complexity: O(log(n))
+# Space complexity: O(1)
+def search(nums: list, target: int) -> int:
+    li, ri = 0, len(nums)-1
+    while li <= ri:
+        mi = (li+ri)//2
+        if target == nums[mi]:
+            return mi
+        if nums[mi] <= nums[ri]:
+            if target > nums[mi] and target <= nums[ri]:
+                li = mi+1
+            else:
+                ri = mi-1
+        else:
+            if target < nums[mi] and target >= nums[li]:
+                ri = mi-1
+            else:
+                li = mi+1
+
+    return -1
