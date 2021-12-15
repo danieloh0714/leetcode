5baef7a4a4019f78f477a9fef05ca83f938550a8
Danny Oh <dannyoh0316@gmail.com>
2021-12-15 00:24:14 -0600

    spacing fixes

--- 0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number.py
@@ -3,14 +3,16 @@
 def letter_combinations(digits: str) -> list:
     if digits == '': return []
 
-    mappings = { '2': 'abc',
+    mappings = {
+            '2': 'abc',
             '3': 'def',
             '4': 'ghi',
             '5': 'jkl',
             '6': 'mno',
             '7': 'pqrs',
             '8': 'tuv',
-            '9': 'wxyz' }
+            '9': 'wxyz'
+            }
 
     combos = []
     def recurse(i, combo):
