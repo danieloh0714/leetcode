698e2689a9f74767612192d0ed079f85598e53d7
Danny Oh <dannyoh0316@gmail.com>
2021-12-17 01:07:36 -0600

    add time and space complexity

--- python/0300_longest_increasing_subsequence.py
+++ python/0300_longest_increasing_subsequence.py
@@ -1,8 +1,9 @@
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def length_of_LIS(nums: list) -> int:
-    ans = 1
-
     dp = [1 for _ in nums]
-    dp[-1] = 1
+
+    ans = 1
     for i in reversed(range(len(nums)-1)):
         for j in range(i+1, len(nums)):
             if nums[j] > nums[i]:
