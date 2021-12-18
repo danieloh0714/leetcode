e6cf7798702e9385c581505411d26316570435c5
Danny Oh <dannyoh0316@gmail.com>
2021-12-17 20:55:01 -0600

    python 0355 design twitter

---
+++ python/0355_design_twitter.py
@@ -0,0 +1,66 @@
+from queue import PriorityQueue
+
+
+class Tweet:
+
+    def __init__(self, time: int, id: int):
+        self.time = time
+        self.id = id
+
+
+class User:
+
+    def __init__(self):
+        self.tweets = []
+        self.following = set()
+
+        
+class Twitter:
+
+    def __init__(self):
+        self.users = dict()
+        self.time = 0
+
+    def post_tweet(self, user_id: int, tweet_id: int) -> None:
+        if user_id not in self.users:
+            self.users[user_id] = User()
+
+        user = self.users[user_id]
+        user.tweets.append(Tweet(self.time, tweet_id))
+        self.time += 1
+
+    def get_news_feed(self, user_id: int) -> list:
+        if user_id not in self.users: return []
+
+        min_heap = PriorityQueue()
+
+        user = self.users[user_id]
+        for followee in user.following:
+            if followee not in self.users: continue
+
+            for tweet in self.users[followee].tweets:
+                min_heap.put((tweet.time, tweet.id))
+                if min_heap.qsize() > 10:
+                    min_heap.get()
+
+        for tweet in user.tweets:
+            min_heap.put((tweet.time, tweet.id))
+            if min_heap.qsize() > 10:
+                min_heap.get()
+
+        feed = []
+        while not min_heap.empty():
+            feed.append(min_heap.get()[1])
+
+        feed.reverse()
+        return feed
+
+    def follow(self, follower_id: int, followee_id: int) -> None:
+        if follower_id not in self.users:
+            self.users[follower_id] = User()
+
+        self.users[follower_id].following.add(followee_id)
+        
+    def unfollow(self, follower_id: int, followee_id: int) -> None:
+        if follower_id in self.users:
+            self.users[follower_id].following.discard(followee_id)
